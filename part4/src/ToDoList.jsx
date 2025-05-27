a27697b7458ad498b10f45105e2772ef5c9170a5
bharatalok11 <bharatalok11@gmail.com>
2025-05-27 23:36:09 +0530

    used uuid npm
    - for assigning unique id to task instead of using index, used uuid npm package to assign unique id

--- part4/src/ToDoList.jsx
+++ part4/src/ToDoList.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ToDoList() {
     let [todos, setTodos] = useState([]);
     let [task,setTask] = useState('');
-    let [editIdx, setEditIdx] = useState(null);
+    let [editId, setEditId] = useState(null);
 
     function updateToDoValue(event){
         setTask(event.target.value);
@@ -12,31 +13,36 @@ export default function ToDoList() {
 
     function handleTaskAdd(event) {
         if(task.trim() === "") return;
-        if(editIdx === null){
-            setTodos(prevList=> [...prevList,task]);
+        if(editId === null){
+            let newTask = {
+                id : uuidv4(),
+                work : task
+            };
+            setTodos(prevList=> [...prevList,newTask]);
         }else{
             //editing existing task
-            setTodos(prevList=>{
-                const updatedList = [...prevList];
-                updatedList[editIdx] = task;
-                return updatedList;
-            });
-            setEditIdx(null);
+            setTodos(prevList =>
+                prevList.map(item =>
+                    item.id === editId ? { ...item, work: task } : item
+                )
+            );
+            setEditId(null);
         }
         setTask("");
     }
 
-    function deleteTask(indexToDelete){
-        setTodos(prevList=> prevList.filter((_,index) => index!=indexToDelete));
+    function deleteTask(idToDelete){
+        setTodos(prevList=> prevList.filter((item) => item.id!=idToDelete));
         // Reset edit if you're deleting the task being edited
-        if (indexToDelete === editIdx) {
+        if (idToDelete === editId) {
             setTask("");
-            setEditIdx(null);
+            setEditId(null);
         }
     }
-    function editTask(indexToEdit){
-        setTask(todos[indexToEdit]);
-        setEditIdx(indexToEdit);
+    function editTask(idToEdit){
+        const itemToEdit = todos.find(item => item.id === idToEdit);
+        setTask(itemToEdit.work);
+        setEditId(idToEdit);
     }
 
     return (
@@ -45,21 +51,24 @@ export default function ToDoList() {
 
             <input onChange={updateToDoValue} placeholder="Enter a task" value={task}/>
             &nbsp; &nbsp; &nbsp;
-            <button onClick={handleTaskAdd}>{editIdx===null ? 'Add' : 'Update'}</button>
+            <button onClick={handleTaskAdd}>{editId===null ? 'Add' : 'Update'}</button>
 
             <hr />
             <h3>Tasks to do : ⤵️</h3>
-            <ul >
-                {
-                    todos.map((item,idx)=>(
-                        <li key={idx}>
-                            {item} &nbsp; &nbsp; &nbsp;
-                            <button onClick={() => editTask(idx)}>Edit</button>
-                            <button onClick={() => deleteTask(idx)}>Delete</button>
-                        </li> )
-                    )
-                }
-            </ul>
+            {todos.length === 0 ? (
+                <p>No tasks yet. Add one! 😊</p>
+                ) : (
+                <ul>
+                    {todos.map(item => (
+                    <li key={item.id}>
+                        {item.work} &nbsp; &nbsp;
+                        <button onClick={() => editTask(item.id)}>Edit</button>
+                        <button onClick={() => deleteTask(item.id)}>Delete</button>
+                    </li>
+                    ))}
+                </ul>
+            )}
+
         </div>
     );
 }
