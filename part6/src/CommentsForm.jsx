0706dd55576945b223a0fac4489138010c0f8689
bharatalok11 <bharatalok11@gmail.com>
2025-05-29 22:43:47 +0530

    used Formik for client-side form validation

--- part6/src/CommentsForm.jsx
+++ part6/src/CommentsForm.jsx
@@ -1,51 +1,62 @@
 import { useState } from "react";
+import { useFormik } from 'formik';
+
+const validate = values => {
+   const errors = {};
+ 
+    if (!values.username) {
+        errors.username = 'username is Required';
+    } else if (values.username.length < 3) {
+        errors.username = 'Must be atleast 3 characters or more';
+    }
+    if (!values.remarks) {
+        errors.remarks = 'please add remark';
+    }
+    if (!values.rating) {
+        errors.rating = 'required';
+    }
+    return errors;
+};
+
 export default function CommentsForm({addNewComment}){
-    let [formData, setFormData] = useState({
-        username: "",
-        remarks: "",
-        rating: 5
-    });  
-
-    let handleInputChange = (event)=>{
-        setFormData((currData) => {
-            return {...currData, [event.target.name]: event.target.value};
-        });
-    }
-
-    let handleFormSubmit = (event)=>{
-        event.preventDefault();
-        // console.log(formData);
-        addNewComment(formData);
-        setFormData({
-            username: "",
-            remarks: "",
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            remarks: '',
             rating: 5
-        }) 
-    }
+        },
+        validate,
+        onSubmit: (values) => {
+            // console.log('Submitted values:', values);
+            addNewComment(values);
+        },
+    });
 
     return(
         <div>
             <h4>Give a Feedback</h4>
-            <form onSubmit={handleFormSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="username">Username : </label>
                 <input
                     type="text"
                     name="username" 
                     id="username" 
-                    value={formData.username}
+                    value={formik.values.username}
                     placeholder="username"
-                    onChange={handleInputChange}
+                    onChange={formik.handleChange}
                 /> 
+                {formik.errors.username ? <div>{formik.errors.username}</div> : null}
                 <br /> <br />
 
                 <label htmlFor="remarks">Remarks : </label>
                 <textarea
                     id="remarks"
                     name="remarks"
-                    value={formData.remarks}
-                    onChange={handleInputChange}
+                    value={formik.values.remarks}
+                    onChange={formik.handleChange}
                     placeholder="Enter your remarks here : "
                 > Remarks </textarea>
+                {formik.errors.remarks ? <div>{formik.errors.remarks}</div> : null}
                 <br /> <br />
 
                 <label htmlFor="rating">Rating : </label>
@@ -53,14 +64,14 @@ export default function CommentsForm({addNewComment}){
                     type="number" 
                     name="rating" 
                     id="rating" 
-                    value={formData.rating}
-                    onChange={handleInputChange}
+                    value={formik.values.rating}
+                    onChange={formik.handleChange}
                     placeholder="Enter rating : "
                     min={1}
                     max={5}
                 />
                 <br /> <br />
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
