3ad97abd238e5a03cf2326dc934482e4e36dd052
bharatalok11 <bharatalok11@gmail.com>
2025-05-31 15:05:58 +0530

    folder initialization
    - installed @reduxjs/toolkit
    - installed react-redux

---
+++ reduxReact/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>Redux React</h1>
+      <p>Welcome to the Redux React application!</p>
+    </div>
+  )
+}
+export default App;
